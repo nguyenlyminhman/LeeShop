812b71bf477cd65c8fdb149e85c30f1b43b3bdee
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-05-25 16:17:15 +0700

    finish index js

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('public'));
+
+app.listen(3000, (req, res) => { console.log('Server is running !!!') });
+app.get('/', (req, res) => {res.render('index')});
