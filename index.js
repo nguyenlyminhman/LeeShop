ae2d773b8bb147561d3f209ed3ce943010fc3604
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-05-25 22:40:57 +0700

    changed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-
+app.listen( process.env.PORT || 3000, () => console.log('Server is running !!!'));
 app.get('/', (req, res) => res.render('home'));
 
-app.listen( process.env.PORT || 3000, (req, res) => console.log('Server is running !!!'));
