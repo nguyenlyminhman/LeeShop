682832188b8052e223dc3031069b92f16c1e1869
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-04 16:02:20 +0700

    front end

--- index.js
+++ index.js
@@ -7,3 +7,11 @@ app.use(express.static('public'));
 app.listen( process.env.PORT || 3000, () => console.log('Server is running !!!'));
 app.get('/', (req, res) => res.render('home'));
 
+app.get('/login', (req, res) => res.render('login'));
+app.get('/register', (req, res) => res.render('register'));
+app.get('/contact', (req, res) => res.render('contact'));
+app.get('/checkout', (req, res) => res.render('checkout'));
+app.get('/mens', (req, res) => res.render('mens'));
+app.get('/womens', (req, res) => res.render('womens'));
+app.get('/single', (req, res) => res.render('single'));
+app.get('/other', (req, res) => res.render('other'));
