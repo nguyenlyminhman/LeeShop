7973d082531189b8240acb0f70bea9da60571197
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-06-09 15:32:38 +0700

    design

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
+app.use('admin', express.static('public/admin'));
 
-app.listen( process.env.PORT || 3000, () => console.log('Server is running !!!'));
+app.listen(process.env.PORT || 3000, () => console.log('Server is running !!!'));
 app.get('/', (req, res) => res.render('home'));
-
+//dieu hướng phần customer
 app.get('/login', (req, res) => res.render('login'));
 app.get('/register', (req, res) => res.render('register'));
 app.get('/contact', (req, res) => res.render('contact'));
@@ -15,6 +16,8 @@ app.get('/mens', (req, res) => res.render('mens'));
 app.get('/womens', (req, res) => res.render('womens'));
 app.get('/single', (req, res) => res.render('single'));
 app.get('/other', (req, res) => res.render('other'));
+//
 
-
+//điều hướng phần admin
 app.get('/manager', (req, res) => res.render('./manager/index'));
+app.get('/admin', (req, res) => res.render('./manager/admin'));
---
+++ model/Category.js
@@ -0,0 +1,11 @@
+
+class Category {
+    constructor(cateId, cateName, cateParent) {
+        this.cateId = cateId;
+        this.cateName = cateName;
+        this.cateParent = cateParent
+    }
+
+    static getProduct
+
+}
